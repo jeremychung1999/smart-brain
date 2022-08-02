f16f6c7c3cb575f6a0adbd1be9e8c024c3d30c3b
jeremychung1999 <jeremychung1999@gmail.com>
2022-08-02 20:57:32 +0800

    first commit

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,17 @@
+import Tilt from 'react-parallax-tilt';
+import brain from './brain.png';
+import './Logo.css';
+
+const Logo = () => {
+	return (
+		<div className="ma4 mt0">
+		 <Tilt className='Tilt br2 shadow-2'>
+	      <div style={{ height: '150px', width: '150px'}}>
+	        <img alt='logo' style={{ height: '100px', width: '100px', padding: '25px'}}src={brain}/>
+	      </div>
+	    </Tilt>
+		</div>
+	);
+}
+
+export default Logo;
